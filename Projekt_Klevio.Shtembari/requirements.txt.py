5f628452a25a10e4cf1573e362a2afb92764f50b
lluka91 <klluka65@gmail.com>
2026-05-31 13:10:19 +0200

    Update and rename requirements.txt.py to requirements.txt

--- Projekt_Klevio.Shtembari/requirements.txt.py
+++
@@ -1,9 +0,0 @@
-%%writefile requirements.txt
-# Paketat kryesore shkencore për modelimin dhe simulimin
-numpy>=2.1.0
-scipy>=1.14.0
-pandas>=2.2.0
-matplotlib>=3.9.0
-
-# Paketat opsionale për menaxhimin e librezave Jupyter (nëse nevojiten)
-ipython>=8.26.0
